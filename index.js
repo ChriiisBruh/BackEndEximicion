6a198b885024c0bee02bc960c21bfd453635788c
chriiisbruh <christian.sanjuan@alumnos.ucn.cl>
2023-09-09 16:45:21 -0300

    post user

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ const { v4: uuidv4 } = require('uuid');
 app.get('/api/users', (req, res) => {
     res.json(users);
   });
+
+app.get('/api/users/:id', (req,res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) return res.status(404).send('Usuario no encontrado');
+    res.json(user);
+});
+
+app.post('/api/users', (req,res) =>{
+    const user = {
+        id: uuidv4(),
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(user);
+    res.status(201).json(user);
+});
