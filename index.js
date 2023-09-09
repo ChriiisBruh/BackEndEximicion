e7755df51492bfebd941d26203b3323c7775965a
chriiisbruh <christian.sanjuan@alumnos.ucn.cl>
2023-09-09 16:40:06 -0300

    get users

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3002;
 const cors = require('cors');
 
 
 app.use(cors());
 app.use(express.json());
 const users = [];
+const reservations = [];
 const { v4: uuidv4 } = require('uuid');
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
+  });
